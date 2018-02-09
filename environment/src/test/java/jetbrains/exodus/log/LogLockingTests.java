8b84e9d6a0812ce5f6f67e1263293d38127d8ad0
Vyacheslav Lukianov <lvo@intellij.net>
2018-02-09 11:02:02 +0300

    update license header

--- environment/src/test/java/jetbrains/exodus/log/LogLockingTests.java
+++ environment/src/test/java/jetbrains/exodus/log/LogLockingTests.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010 - 2017 JetBrains s.r.o.
+ * Copyright 2010 - 2018 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 package jetbrains.exodus.log;
 
 import jetbrains.exodus.ExodusException;
-import jetbrains.exodus.io.DataWriter;
 import org.junit.Assert;
 import org.junit.Test;
 
